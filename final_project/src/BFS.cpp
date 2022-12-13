2ccd45d85f911d076de2a626f243dd8020545c78
BobJ-coder <73429872+BobJ-coder@users.noreply.github.com>
2022-12-12 21:17:20 -0600

    Add files via upload

--- final_project/src/BFS.cpp
+++ final_project/src/BFS.cpp
@@ -2,6 +2,17 @@
 
 BFS::~BFS() {}
 
+BFS::BFS(Graph G): G_(G) {
+    vector<list<pair<int,float>>> vec(G.getSize(), list<pair<int,float>>());
+    for(int i = 0; i < G.getSize(); i++){
+        list<pair<int, float>> adjacent = G.getAdjacentVertices(i);
+        for(auto j = adjacent.begin(); j != adjacent.end(); j++){
+            vec[j->first].push_back({i, j->second});
+        }
+    }
+}
+
+
 std::vector<std::string> BFS::search(int i) {
     std::vector<bool> visited;
     visited.resize(G_.getSize());
@@ -24,4 +35,28 @@ std::vector<std::string> BFS::search(int i) {
         }
     }
     return airports;
+}
+
+int BFS::search(int start, int end) {
+    std::vector<bool> visited;
+    visited.resize(G_.getSize());
+    for (auto a : visited) {
+        a = false;
+    }
+    int flights = 0;
+    list<int> queue;
+    visited[start] = true;
+    queue.push_back(start);
+    while(queue.front() != end) {
+        start = queue.front();
+        flights++;
+        queue.pop_front();
+        for (auto adj : G_.getAdjacentVertices(start)) {
+            if (visited[adj.first] == false) {
+                queue.push_back(adj.first);
+                visited[adj.first] = true;
+            }
+        }
+    }
+    return flights;
 }
--- final_project/src/BFS.h
+++ final_project/src/BFS.h
@@ -19,6 +19,11 @@ class BFS {
     * BFS algorithm 
     */
     std::vector<std::string> search(int start);
+    /**
+    * BFS algorithm for finding the flights one needs to take from one airport to another. 
+    * Start is the the airport one departs from, and end is the destination.
+    */
+    int search(int start, int end);
     private:
     Graph G_;    
 };
